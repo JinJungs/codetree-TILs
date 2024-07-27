f27461fcb4b2564c1ed90b9ab9b428914f916d60
Eui Jin Jung <nehciyz@gmail.com>
2024-07-27 23:46:39 +0900

    Title: DFS와 BFS, Time: 188ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240727/DFS와 BFS/DFS-and-BFS.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+    graph[x].sort()
+    graph[y].sort()
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력하기
+        v = queue.popleft()
+        print(v, end=' ')
+        # 아직 방문하지 않은 인접 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+dfs(graph, 1, [False] * (n+1))
+print('')
+bfs(graph, 1, [False] * (n+1))
