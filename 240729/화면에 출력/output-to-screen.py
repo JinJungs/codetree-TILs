a9fb1dbce97b10de13c68cce61706ba5fb0e1949
Eui Jin Jung <nehciyz@gmail.com>
2024-07-29 00:12:47 +0900

    Title: 화면에 출력, Time: 978ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240729/화면에 출력/output-to-screen.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+s = int(input())
+
+def bfs(len_init, s):
+    q = deque([(len_init, len_init)])
+
+    result = 1
+    while q:
+        sz = len(q)
+        for _ in range(sz):
+            screen, clipboard = q.popleft()
+            if screen == s:
+                return result
+
+            # paste character in clipboard to screen
+            q.append((screen + clipboard, clipboard))
+
+            # delete one character in screen
+            q.append((screen - 1, clipboard))
+
+            # copy all characters in screen to clipboard
+            q.append((screen, screen))
+
+        result += 1
+
+    return 0
+
+print(bfs(1,s))
