c317963dc883805a6ed037eba96fb5486bf76dc2
Eui Jin Jung <nehciyz@gmail.com>
2024-07-30 21:32:47 +0900

    Title: 색맹, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240730/색맹/color-blindness.py
+++ 240730/색맹/color-blindness.py
@@ -39,7 +39,7 @@ def loop_dfs(grid, visited) -> int:
     for x in range(n):
         for y in range(n):
             if not visited[x][y]:
-                iterative_dfs(grid, x, y, visited)
+                dfs(grid, x, y, visited)
                 region += 1
     return region
 
