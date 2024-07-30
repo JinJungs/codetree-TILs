5c60528547640f277279439b23c705449f5ddf5e
Eui Jin Jung <nehciyz@gmail.com>
2024-07-30 21:21:40 +0900

    Title: 색맹, Time: 160ms, Memory: 28MB, Status: Passed - CodeTree

--- 240730/색맹/color-blindness.py
+++ 240730/색맹/color-blindness.py
@@ -14,22 +14,34 @@ def in_range(x,y) -> bool:
 
 # how to distinguish between the first time of the block and the end of it?
 # for loop should be inside of it
-def dfs(grid, x, y, visited):
+def dfs(grid, x, y, visited) -> int:
     visited[x][y] = True
     result = 1
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-        if in_range(nx,ny) and not visited[nx][ny] and grid[x][y] == grid[nx][ny] :
+        if in_range(nx,ny) and not visited[nx][ny] and grid[x][y] == grid[nx][ny]:
             result += dfs(grid, nx, ny, visited)
     return result
 
+def iterative_dfs(grid, x, y, visited):
+    stack = [(x, y)]
+    visited[x][y] = True
+    while stack:
+        cx, cy = stack.pop()
+        for i in range(4):
+            nx, ny = cx + dx[i], cy + dy[i]
+            if in_range(nx, ny) and not visited[nx][ny] and grid[cx][cy] == grid[nx][ny]:
+                visited[nx][ny] = True
+                stack.append((nx, ny))
+
 def loop_dfs(grid, visited) -> int:
-    district = []
+    region = 0
     for x in range(n):
         for y in range(n):
             if not visited[x][y]:
-                district.append(dfs(grid, x, y, visited))
-    return len(district)
+                iterative_dfs(grid, x, y, visited)
+                region += 1
+    return region
 
 
 print(loop_dfs(normal_grid, normal_visited), end=' ')
